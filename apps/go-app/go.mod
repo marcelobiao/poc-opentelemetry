732f4af4512d9a5ede1978adb8e70cb9077db8cc
marcelobiao <biao.marcelo@gmail.com>
2023-08-14 14:00:33 -0300

    feat: melhorando poc otel+golang

--- apps/go-app/go.mod
+++ apps/go-app/go.mod
@@ -3,26 +3,25 @@ module github.com/marcelobiao/poc-opentelemetry/apps/go-app
 go 1.20
 
 require (
+	github.com/gofiber/contrib/otelfiber v1.0.10
 	github.com/gofiber/fiber/v2 v2.48.0
-	github.com/google/uuid v1.3.0
 	go.opentelemetry.io/otel v1.16.0
 	go.opentelemetry.io/otel/exporters/zipkin v1.16.0
 	go.opentelemetry.io/otel/sdk v1.16.0
 	go.opentelemetry.io/otel/trace v1.16.0
-	gorm.io/gorm v1.25.3
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/gofiber/contrib/otelfiber/v2 v2.0.0-20230811211426-8f9e28e17f38 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/openzipkin/zipkin-go v0.4.1 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.48.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- apps/go-app/main.go
+++ apps/go-app/main.go
@@ -1,69 +1,71 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/gofiber/contrib/otelfiber/v2"
+	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
-	"github.com/marcelobiao/poc-opentelemetry/apps/go-app/infra/opentelemetry"
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/zipkin"
-	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
-var Exporter *zipkin.Exporter
+var tracer = otel.Tracer("")
 
 func main() {
+	// Configurando exporter
 	zipkinConn, err := zipkin.New("http://localhost:9411/api/v2/spans")
 	if err != nil {
 		log.Fatal(err)
 	}
-	Exporter = zipkinConn
 
-	app := fiber.New(
-		fiber.Config{
-			Immutable: true,
-			BodyLimit: 50 * 1024 * 1024,
-		},
-	)
-	app.Use(MyMiddleware())
+	// Configurando tracer
+	tp := initTracer(zipkinConn)
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
+	// Configurando app fiber
+	app := fiber.New()
+	app.Use(otelfiber.Middleware())
 	app.Get("/", homeHandler)
 	if err := app.Listen("localhost:8000"); err != nil {
 		panic(err)
 	}
 }
 
-func MyMiddleware() fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		id := uuid.New()
-		provider := opentelemetry.GetTracer(Exporter, id.String())
-		tracer := provider.Tracer("go-app")
-		c.Locals("provider", provider)
-		c.Locals("tracer", tracer)
-		otelMiddleware := otelfiber.Middleware(otelfiber.WithTracerProvider(provider))
-		err := otelMiddleware(c)
-		return err
-	}
+func initTracer(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("go-app"),
+			)),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
 }
 
 func homeHandler(c *fiber.Ctx) error {
-	// provider := c.Locals("provider").(*trace.TracerProvider)
-	tracer := c.Locals("tracer").(trace.Tracer)
 	time.Sleep(time.Millisecond * 50)
-
-	_, processFile := tracer.Start(c.Context(), "process-file")
-	processFile.SetAttributes(attribute.String("info", "top"))
+	_, span := tracer.Start(c.UserContext(), "HomeHandler", oteltrace.WithAttributes(attribute.String("id", "testeteste")))
+	defer span.End()
+	span.SetAttributes(attribute.String("info", "texto"))
 	time.Sleep(time.Millisecond * 50)
 	c.SendStatus(http.StatusAccepted)
 	c.JSON("home")
-	processFile.End()
 	return nil
-}
-
-func testHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("test"))
 }
