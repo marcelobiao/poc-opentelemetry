0fada5142e73ddd951bc26afb864ccc40a79c24b
marcelobiao <biao.marcelo@gmail.com>
2023-09-16 16:58:59 -0300

    feat: add manual instrumentation on service-2

--- apps/go-app-2/app/server.go
+++ apps/go-app-2/app/server.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
 )
 
 func StartGinWebServer() {
@@ -26,11 +28,19 @@ func StartFiberWebServer() {
 	app.Use(otelfiber.Middleware())
 
 	app.Get("/ping", func(c *fiber.Ctx) error {
+		tp := otel.GetTracerProvider()
+		_, span1 := tp.Tracer("").Start(c.UserContext(), "stage1")
+		defer span1.End()
+		time.Sleep(time.Microsecond * 150)
+		_, span2 := tp.Tracer("").Start(c.UserContext(), "stage2")
+		time.Sleep(time.Microsecond * 50)
+		span2.End()
+		time.Sleep(time.Microsecond * 50)
 		c.SendStatus(http.StatusOK)
 		c.JSON("pong")
 		return nil
 	})
-	if err := app.Listen(":8091"); err != nil {
+	if err := app.Listen(":8090"); err != nil {
 		panic(err)
 	}
 }
